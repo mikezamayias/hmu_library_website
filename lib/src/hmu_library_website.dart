1c6adcade2bcae3edb40f9d3dd51f89b8ea037b5
Mike Zamayias <mzamagias@gmail.com>
2021-11-30 06:03:08 +0200

    trying to get something on the screen

--- lib/src/hmu_library_website.dart
+++ lib/src/hmu_library_website.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hmu_library_website/src/home_page/home_page.dart';
 
 class HMULibraryWebsite extends StatefulWidget {
   const HMULibraryWebsite({Key? key}) : super(key: key);
@@ -10,6 +11,11 @@ class HMULibraryWebsite extends StatefulWidget {
 class _HMULibraryWebsiteState extends State<HMULibraryWebsite> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const MaterialApp(
+      title: 'HMU Library Website',
+      home: SafeArea(
+        child: HomePage(),
+      ),
+    );
   }
 }
