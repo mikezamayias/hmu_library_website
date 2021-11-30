a7abb760b43edb3523a4f52671f475583f5c0100
Mike Zamayias <mzamagias@gmail.com>
2021-11-30 04:31:21 +0200

    refactor

---
+++ lib/src/hmu_library_website.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HMULibraryWebsite extends StatefulWidget {
+  const HMULibraryWebsite({Key? key}) : super(key: key);
+
+  @override
+  _HMULibraryWebsiteState createState() => _HMULibraryWebsiteState();
+}
+
+class _HMULibraryWebsiteState extends State<HMULibraryWebsite> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hmu_library_website/main.dart';
+import 'package:hmu_library_website/src/hmu_library_website.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const HMULibraryWebsite());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
