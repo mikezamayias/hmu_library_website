8819c4628cb33dd8794758a7089dc88949ad1330
Mike Zamayias <mzamagias@gmail.com>
2021-11-30 06:02:56 +0200

    initial navigation bar

---
+++ lib/src/widgets/navigation_bar.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class NavigationBar extends StatefulWidget {
+  const NavigationBar({Key? key}) : super(key: key);
+
+  @override
+  _NavigationBarState createState() => _NavigationBarState();
+}
+
+class _NavigationBarState extends State<NavigationBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 90,
+      color: Colors.orange,
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+      ),
+    );
+  }
+}
