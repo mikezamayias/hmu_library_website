6bdc83c3b6e29239364cfba8fd4cb95a599624c2
Mike Zamayias <mzamagias@gmail.com>
2021-11-30 07:49:09 +0200

    minor refactor

--- lib/src/themes/text_theme.dart
+++ lib/src/themes/text_theme.dart
@@ -2,42 +2,42 @@ import 'package:flutter/material.dart';
 
 TextTheme textTheme = const TextTheme(
   headline1: TextStyle(
-    color: Color(0xff303030),
+    color: Color(0xFF303030),
   ),
   headline2: TextStyle(
-    color: Color(0xff303030),
+    color: Color(0xFF303030),
   ),
   headline3: TextStyle(
-    color: Color(0xff303030),
+    color: Color(0xFF303030),
   ),
   headline4: TextStyle(
-    color: Color(0xff303030),
+    color: Color(0xFF303030),
   ),
   headline5: TextStyle(
-    color: Color(0xff303030),
+    color: Color(0xFF303030),
   ),
   headline6: TextStyle(
-    color: Color(0xff303030),
+    color: Color(0xFF303030),
   ),
   subtitle1: TextStyle(
-    color: Color(0xff303030),
+    color: Color(0xFF303030),
   ),
   subtitle2: TextStyle(
-    color: Color(0xff303030),
+    color: Color(0xFF303030),
   ),
   bodyText1: TextStyle(
-    color: Color(0xff303030),
+    color: Color(0xFF303030),
   ),
   bodyText2: TextStyle(
-    color: Color(0xff303030),
+    color: Color(0xFF303030),
   ),
   caption: TextStyle(
-    color: Color(0xff303030),
+    color: Color(0xFF303030),
   ),
   button: TextStyle(
-    color: Color(0xff303030),
+    color: Color(0xFF303030),
   ),
   overline: TextStyle(
-    color: Color(0xff303030),
+    color: Color(0xFF303030),
   ),
 );
