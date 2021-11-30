c02e7aeceea7e10ac384879b60cb81b65a52813c
Mike Zamayias <mzamagias@gmail.com>
2021-11-30 06:25:27 +0200

    added navigation bar buttons

--- lib/src/widgets/navigation_bar.dart
+++ lib/src/widgets/navigation_bar.dart
@@ -10,11 +10,38 @@ class NavigationBar extends StatefulWidget {
 class _NavigationBarState extends State<NavigationBar> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 90,
-      color: Colors.orange,
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.1,
       child: Row(
         mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          TextButton.icon(
+            onPressed: () {},
+            icon: const Icon(Icons.home_rounded),
+            label: const Text('Home'),
+          ),
+          TextButton.icon(
+            onPressed: () {},
+            icon: const Icon(Icons.person_add_rounded),
+            label: const Text('Sign Up'),
+          ),
+          TextButton.icon(
+            onPressed: () {},
+            icon: const Icon(Icons.login_rounded),
+            label: const Text('Login'),
+          ),
+          TextButton.icon(
+            onPressed: () {},
+            icon: const Icon(Icons.library_books_rounded),
+            label: const Text('Book Catalog'),
+          ),
+          TextButton.icon(
+            onPressed: () {},
+            icon: const Icon(Icons.alternate_email_rounded),
+            label: const Text('Contact'),
+          ),
+        ],
       ),
     );
   }
