ea71a8ae56c47278e1ec5703cc66a22b7ca2f4ec
Mike Zamayias <mzamagias@gmail.com>
2021-12-01 23:29:20 +0200

    removed background image

--- lib/src/home_page/greetings_section.dart
+++ lib/src/home_page/greetings_section.dart
@@ -11,44 +11,32 @@ class GreetingsSection extends StatefulWidget {
 class _GreetingsSectionState extends State<GreetingsSection> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: const AssetImage('assets/images/hmu_lib_5.jpg'),
-          fit: BoxFit.cover,
-          colorFilter: ColorFilter.mode(
-            Colors.white.withOpacity(0.72),
-            BlendMode.lighten,
-          ),
-        ),
-      ),
-      child: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(60),
-          child: SizedBox(
-            width: MediaQuery.of(context).size.width * 0.6,
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SelectableText(
-                  greeting.title,
-                  style: const TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.w500,
-                  ),
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(60),
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width * 0.8,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SelectableText(
+                greeting.title,
+                style: const TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w500,
                 ),
-                const SizedBox(height: 30),
-                SelectableText(
-                  greeting.text,
-                  style: const TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w400,
-                  ),
+              ),
+              const SizedBox(height: 30),
+              SelectableText(
+                greeting.text,
+                style: const TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w400,
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
