c6e43d5b45cec68960636cc72fb8c2bbbca18d82
Mike Zamayias <mzamagias@gmail.com>
2021-12-01 23:02:41 +0200

    refactored

--- lib/src/hmu_library_website.dart
+++ lib/src/hmu_library_website.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import 'package:hmu_library_website/src/home_page/home_page.dart';
+import 'package:hmu_library_website/src/widgets/navigation_bar_text_button.dart';
 
 import 'themes/current_theme.dart';
 
@@ -24,8 +26,60 @@ class _HMULibraryWebsiteState extends State<HMULibraryWebsite> {
       darkTheme: CurrentTheme.darkTheme,
       themeMode: currentTheme.currentTheme,
       title: 'HMU Library Website',
-      home: const SafeArea(
-        child: HomePage(),
+      home: SafeArea(
+        child: Scaffold(
+          body: Scrollbar(
+            child: CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  automaticallyImplyLeading: false,
+                  centerTitle: false,
+                  backgroundColor: const Color(0xFFA9915D),
+                  shadowColor: const Color(0xFF1A4859),
+                  elevation: 15,
+                  forceElevated: true,
+                  toolbarHeight: 75,
+                  pinned: false,
+                  floating: true,
+                  actions: [
+                    NavigationBarTextButton(
+                      label: 'Home',
+                      icon: Icons.home_rounded,
+                      onPressed: () {},
+                    ),
+                    NavigationBarTextButton(
+                      label: 'Sign Up',
+                      icon: Icons.person_add_rounded,
+                      onPressed: () {},
+                    ),
+                    NavigationBarTextButton(
+                      label: 'Login',
+                      icon: Icons.login_rounded,
+                      onPressed: () {},
+                    ),
+                    NavigationBarTextButton(
+                      label: 'Book Catalog',
+                      icon: Icons.library_books_rounded,
+                      onPressed: () {},
+                    ),
+                    NavigationBarTextButton(
+                      label: 'Contact',
+                      icon: Icons.alternate_email_outlined,
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+                SliverList(
+                  delegate: SliverChildListDelegate(
+                    [
+                      const HomePage(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/src/home_page/home_page.dart
+++ lib/src/home_page/home_page.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hmu_library_website/src/home_page/body_information.dart';
-import 'package:hmu_library_website/src/widgets/navigation_bar_text_button.dart';
 
 import 'greetings_section.dart';
 import '../widgets/footer.dart';
-import '../widgets/navigation_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -16,67 +14,12 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Scrollbar(
-        child: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              backgroundColor: const Color(0xFFEEE9DF),
-              leading: const FittedBox(
-                fit: BoxFit.scaleDown,
-                child: Image(
-                  image: AssetImage('assets/images/hmu_logo.png'),
-                ),
-              ),
-              title: const Text(
-                'HMU Library',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFF1A4859),
-                ),
-              ),
-              pinned: true,
-              actions: [
-                NavigationBarTextButton(
-                  label: 'Home',
-                  icon: Icons.home_rounded,
-                  onPressed: () {},
-                ),
-                NavigationBarTextButton(
-                  label: 'Sign Up',
-                  icon: Icons.person_add_rounded,
-                  onPressed: () {},
-                ),
-                NavigationBarTextButton(
-                  label: 'Login',
-                  icon: Icons.login_rounded,
-                  onPressed: () {},
-                ),
-                NavigationBarTextButton(
-                  label: 'Book Catalog',
-                  icon: Icons.library_books_rounded,
-                  onPressed: () {},
-                ),
-                NavigationBarTextButton(
-                  label: 'Contact',
-                  icon: Icons.alternate_email_outlined,
-                  onPressed: () {},
-                ),
-              ],
-            ),
-            SliverToBoxAdapter(
-              child: Column(
-                children: const [
-                  GreetingsSection(),
-                  BodyInformation(),
-                  Footer(),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+    return Column(
+      children: const [
+        GreetingsSection(),
+        BodyInformation(),
+        Footer(),
+      ],
     );
   }
 }
