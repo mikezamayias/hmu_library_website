a45fe847c8ab9da1880ac7d7ac908be4e4a1a247
Mike Zamayias <mzamagias@gmail.com>
2021-12-01 09:26:41 +0200

    changed to elevated button

--- lib/src/widgets/navigation_bar_text_button.dart
+++ lib/src/widgets/navigation_bar_text_button.dart
@@ -3,12 +3,14 @@ import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 class NavigationBarTextButton extends StatefulWidget {
   final VoidCallback? onPressed;
-  final String? string;
+  final String? label;
+  final IconData? icon;
 
   const NavigationBarTextButton({
     Key? key,
     this.onPressed,
-    this.string,
+    this.label,
+    this.icon,
   }) : super(key: key);
 
   @override
@@ -19,24 +21,45 @@ class NavigationBarTextButton extends StatefulWidget {
 class _NavigationBarTextButtonState extends State<NavigationBarTextButton> {
   @override
   Widget build(BuildContext context) {
-    return NeumorphicButton(
+    return ElevatedButton.icon(
       onPressed: widget.onPressed,
-      child: Text(
-        widget.string ?? '',
-        style: const TextStyle(
-          fontSize: 18,
-          fontWeight: FontWeight.w500,
-          color: Colors.white,
-        ),
-      ),
-      style: NeumorphicStyle(
-        shape: NeumorphicShape.flat,
-        boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(60)),
-        depth: 6,
-        color: const Color(0xFFA9915D),
-        lightSource: LightSource.top,
-        shadowDarkColor: const Color(0xFF1A4859),
+      icon: Icon(widget.icon),
+      label: Text(
+        widget.label!,
       ),
     );
+    // return GestureDetector(
+    //   onTap: widget.onPressed,
+    //   child: Container(
+    //     decoration: BoxDecoration(
+    //       borderRadius: BorderRadius.circular(30),
+    //       border: const Border(
+    //         bottom: BorderSide(color: Color(0xFF1A4859), width: 3),
+    //         top: BorderSide(color: Color(0xFF1A4859), width: 3),
+    //         right: BorderSide(color: Color(0xFF1A4859), width: 3),
+    //         left: BorderSide(color: Color(0xFF1A4859), width: 3),
+    //       ),
+    //       color: const Color(0xFFA9915D),
+    //       boxShadow: [
+    //         BoxShadow(
+    //           color: const Color(0xFF1A4859).withOpacity(0.15),
+    //           blurRadius: 10,
+    //           spreadRadius: 5,
+    //         ),
+    //       ],
+    //     ),
+    //     child: Padding(
+    //       padding: const EdgeInsets.all(15),
+    //       child: Text(
+    //         widget.string ?? '',
+            // style: const TextStyle(
+            //   fontSize: 18,
+            //   fontWeight: FontWeight.w500,
+            //   color: Colors.white,
+            // ),
+    //       ),
+    //     ),
+    //   ),
+    // );
   }
 }
