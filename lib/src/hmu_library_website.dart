8eee1999c2dd25eac46bd4fcf253631cdf8ace69
Mike Zamayias <mzamagias@gmail.com>
2021-12-01 23:54:28 +0200

    refactored to use previous navigation bar

--- lib/src/hmu_library_website.dart
+++ lib/src/hmu_library_website.dart
@@ -32,64 +32,12 @@ class _HMULibraryWebsiteState extends State<HMULibraryWebsite> {
           body: Scrollbar(
             child: CustomScrollView(
               slivers: [
-                SliverAppBar(
-                  flexibleSpace: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Center(
-                      child: Row(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Visibility(
-                            visible: false,
-                            child: NavigationBarTextButton(
-                              label: 'Home',
-                              icon: Icons.home_rounded,
-                              onPressed: () {},
-                            ),
-                          ),
-                          NavigationBarTextButton(
-                            label: 'Home',
-                            icon: Icons.home_rounded,
-                            onPressed: () {},
-                          ),
-                          NavigationBarTextButton(
-                            label: 'Sign Up',
-                            icon: Icons.person_add_rounded,
-                            onPressed: () {},
-                          ),
-                          NavigationBarTextButton(
-                            label: 'Login',
-                            icon: Icons.login_rounded,
-                            onPressed: () {},
-                          ),
-                          NavigationBarTextButton(
-                            label: 'Book Catalog',
-                            icon: Icons.library_books_rounded,
-                            onPressed: () {},
-                          ),
-                          NavigationBarTextButton(
-                            label: 'Contact',
-                            icon: Icons.alternate_email_outlined,
-                            onPressed: () {},
-                          ),
-                          Visibility(
-                            visible: false,
-                            child: NavigationBarTextButton(
-                              label: 'Home',
-                              icon: Icons.home_rounded,
-                              onPressed: () {},
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
+                const SliverAppBar(
+                  flexibleSpace: NavigationBar(),
                   automaticallyImplyLeading: false,
                   centerTitle: false,
-                  backgroundColor: const Color(0xFFA9915D),
-                  shadowColor: const Color(0xFF1A4859),
+                  backgroundColor: Color(0xFFA9915D),
+                  shadowColor: Color(0xFF1A4859),
                   elevation: 15,
                   forceElevated: true,
                   floating: true,
