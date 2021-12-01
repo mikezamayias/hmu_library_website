6164bb705f9efabdba9dbffc2faecd3a60a864f1
Mike Zamayias <mzamagias@gmail.com>
2021-12-02 00:07:30 +0200

    ui tweaks

--- lib/src/themes/text_button_theme.dart
+++ lib/src/themes/text_button_theme.dart
@@ -2,13 +2,24 @@ import 'package:flutter/material.dart';
 
 TextButtonThemeData textButtonTheme = TextButtonThemeData(
   style: ButtonStyle(
-    backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
+    padding: MaterialStateProperty.all< EdgeInsetsGeometry>(
+      const EdgeInsets.all(15),
+    ),
+    backgroundColor: MaterialStateProperty.all<Color>(
+      Colors.white,
+    ),
     textStyle: MaterialStateProperty.all<TextStyle>(
       const TextStyle(
         fontSize: 30.0,
         fontWeight: FontWeight.w500,
-        color: Color(0xFF303030),
+        color: Color(0xFF1A4859),
       ),
+    ),
+    foregroundColor: MaterialStateProperty.all<Color>(
+      Colors.white,
+    ),
+    overlayColor: MaterialStateProperty.all<Color>(
+      const Color(0xFF1A4859).withOpacity(0.1),
     ),
     shape: MaterialStateProperty.all<OutlinedBorder>(
       const RoundedRectangleBorder(
