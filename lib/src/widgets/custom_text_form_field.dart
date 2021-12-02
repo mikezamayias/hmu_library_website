e3d2807144d636c61ba855a1937544172c25be2d
Mike Zamayias <mzamagias@gmail.com>
2021-12-03 00:10:30 +0200

    added more field to custom text form field

--- lib/src/widgets/custom_text_form_field.dart
+++ lib/src/widgets/custom_text_form_field.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:hmu_library_website/src/model/validators.dart';
 
 class CusotmTextFormField extends StatefulWidget {
   final String labelText;
   final bool? obscureText;
+  final TextEditingController? controller;
+  final String? validator;
+  final ValidatorType? validatorType;
+  final String? confrimPassword;
 
   const CusotmTextFormField({
     Key? key,
     required this.labelText,
     this.obscureText,
+    this.controller,
+    this.validator,
+    this.validatorType,
+    this.confrimPassword,
   }) : super(key: key);
 
   @override
@@ -26,6 +35,12 @@ class _CusotmTextFormFieldState extends State<CusotmTextFormField> {
             labelText: widget.labelText,
           ),
           obscureText: widget.obscureText ?? false,
+          controller: widget.controller,
+          validator: (value) => validate(
+            value!,
+            widget.validatorType!,
+            widget.confrimPassword!,
+          ),
         ),
       ),
     );
