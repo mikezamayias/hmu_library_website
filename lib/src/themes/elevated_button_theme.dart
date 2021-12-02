4073f51d6070b2d0167711592fdc06d68d5a2729
Mike Zamayias <mzamagias@gmail.com>
2021-12-02 22:38:58 +0200

    tweaked color

--- lib/src/themes/elevated_button_theme.dart
+++ lib/src/themes/elevated_button_theme.dart
@@ -2,8 +2,21 @@ import 'package:flutter/material.dart';
 
 ElevatedButtonThemeData elevatedButtonThemeData = ElevatedButtonThemeData(
   style: ButtonStyle(
+    elevation: MaterialStateProperty.all<double>(6),
     padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
       const EdgeInsets.all(24),
+    ),
+    overlayColor: MaterialStateProperty.resolveWith<Color?>(
+      (Set<MaterialState> states) {
+        if (states.contains(MaterialState.hovered)) {
+          return const Color(0xFF1A4859);
+        }
+        if (states.contains(MaterialState.focused) ||
+            states.contains(MaterialState.pressed)) {
+          return const Color(0xFFA9915D);
+        }
+        return null;
+      },
     ),
     backgroundColor: MaterialStateProperty.all<Color>(
       const Color(0xFFA9915D),
@@ -13,13 +26,21 @@ ElevatedButtonThemeData elevatedButtonThemeData = ElevatedButtonThemeData(
         borderRadius: BorderRadius.all(Radius.circular(60)),
       ),
     ),
-    shadowColor: MaterialStateProperty.all<Color>(
-      const Color(0xFF1A4859).withOpacity(0.15),
+    shadowColor: MaterialStateProperty.resolveWith<Color?>(
+      (Set<MaterialState> states) {
+        if (states.contains(MaterialState.hovered)) {
+          return const Color(0xFF1A4859);
+        }
+        if (states.contains(MaterialState.focused) ||
+            states.contains(MaterialState.pressed)) {
+          return const Color(0xFFA9915D);
+        }
+        return null;
+      },
     ),
-    side: MaterialStateProperty.all<BorderSide>(const BorderSide(
-      color: Color(0xFF1A4859),
-      width: 3,
-    )),
+    side: MaterialStateProperty.all<BorderSide>(
+      const BorderSide(color: Color(0xFF1A4859), width: 3),
+    ),
     textStyle: MaterialStateProperty.all<TextStyle>(
       const TextStyle(
         fontSize: 21,
