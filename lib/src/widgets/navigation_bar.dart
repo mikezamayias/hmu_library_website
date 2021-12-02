488b19f87c3f0dcd59044234c31d7c77577be567
Mike Zamayias <mzamagias@gmail.com>
2021-12-02 16:28:28 +0200

    disabled page route animation

--- lib/src/widgets/navigation_bar.dart
+++ lib/src/widgets/navigation_bar.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hmu_library_website/src/pages/book_catalog_page/book_catalog_page.dart';
+import 'package:hmu_library_website/src/pages/contact_page/contact_page.dart';
+import 'package:hmu_library_website/src/pages/home_page/home_page.dart';
+import 'package:hmu_library_website/src/pages/login_page/login_page.dart';
+import 'package:hmu_library_website/src/pages/sign_up_page/sign_up_page.dart';
 
 import 'navigation_bar_text_button.dart';
 
@@ -23,40 +28,89 @@ class _NavigationBarState extends State<NavigationBar> {
             child: NavigationBarTextButton(
               label: 'Home',
               icon: Icons.home_rounded,
-              onPressed: () => Navigator.pushNamed(context, '/home'),
+              onPressed: () => Navigator.pushReplacement(
+                context,
+                PageRouteBuilder(
+                  pageBuilder: (context, animation1, animation2) =>
+                      const HomePage(),
+                  transitionDuration: Duration.zero,
+                ),
+              ),
             ),
           ),
           NavigationBarTextButton(
             label: 'Home',
             icon: Icons.home_rounded,
-            onPressed: () => Navigator.pushNamed(context, '/home'),
+            onPressed: () => Navigator.pushReplacement(
+              context,
+              PageRouteBuilder(
+                pageBuilder: (context, animation1, animation2) =>
+                    const HomePage(),
+                transitionDuration: Duration.zero,
+              ),
+            ),
           ),
           NavigationBarTextButton(
             label: 'Sign Up',
             icon: Icons.person_add_rounded,
-            onPressed: () => Navigator.pushNamed(context, '/signup'),
+            onPressed: () => Navigator.pushReplacement(
+              context,
+              PageRouteBuilder(
+                pageBuilder: (context, animation1, animation2) =>
+                    const SignUpPage(),
+                transitionDuration: Duration.zero,
+              ),
+            ),
           ),
           NavigationBarTextButton(
             label: 'Login',
             icon: Icons.login_rounded,
-            onPressed: () => Navigator.pushNamed(context, '/login'),
+            onPressed: () => Navigator.pushReplacement(
+              context,
+              PageRouteBuilder(
+                pageBuilder: (context, animation1, animation2) =>
+                    const LoginPage(),
+                transitionDuration: Duration.zero,
+              ),
+            ),
           ),
           NavigationBarTextButton(
             label: 'Book Catalog',
             icon: Icons.library_books_rounded,
-            onPressed: () => Navigator.pushNamed(context, '/bookcatalog'),
+            onPressed: () => Navigator.pushReplacement(
+              context,
+              PageRouteBuilder(
+                pageBuilder: (context, animation1, animation2) =>
+                    const BookCatalogPage(),
+                transitionDuration: Duration.zero,
+              ),
+            ),
           ),
           NavigationBarTextButton(
             label: 'Contact',
             icon: Icons.alternate_email_outlined,
-            onPressed: () => Navigator.pushNamed(context, '/contact'),
+            onPressed: () => Navigator.pushReplacement(
+              context,
+              PageRouteBuilder(
+                pageBuilder: (context, animation1, animation2) =>
+                    const ContactPage(),
+                transitionDuration: Duration.zero,
+              ),
+            ),
           ),
           Visibility(
             visible: false,
             child: NavigationBarTextButton(
               label: 'Home',
               icon: Icons.home_rounded,
-              onPressed: () => Navigator.pushNamed(context, '/home'),
+              onPressed: () => Navigator.pushReplacement(
+                context,
+                PageRouteBuilder(
+                  pageBuilder: (context, animation1, animation2) =>
+                      const HomePage(),
+                  transitionDuration: Duration.zero,
+                ),
+              ),
             ),
           ),
         ],
