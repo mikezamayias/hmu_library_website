da0f73aae976c1cdba7d1d2cd72658d069d41065
Mike Zamayias <mzamagias@gmail.com>
2021-12-07 18:38:52 +0200

    more book information widget

---
+++ lib/src/widgets/book_field_information.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class BookFieldInformation extends StatelessWidget {
+  final String header;
+  final String content;
+
+  const BookFieldInformation({
+    Key? key,
+    required this.header,
+    required this.content,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: SelectableText(
+        header,
+        style: const TextStyle(
+          color: Color(0xFF1A4859),
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      subtitle: SelectableText(
+        content,
+        style: TextStyle(
+          color: const Color(0xFF1A4859),
+          fontSize: 21,
+          fontFamily: GoogleFonts.gfsDidot().fontFamily,
+        ),
+      ),
+    );
+  }
+}
