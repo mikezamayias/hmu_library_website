26dab89bcc3e4b4e908777bf7f0456debae6382a
Mike Zamayias <mzamagias@gmail.com>
2021-12-07 18:38:35 +0200

    added feature
    where when user clicks the book card, more information about the book shows up in a dialog

--- lib/src/widgets/book_card.dart
+++ lib/src/widgets/book_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:hmu_library_website/src/widgets/book_field_information.dart';
 
 class BookCard extends StatefulWidget {
   final String? title;
@@ -26,67 +27,101 @@ class BookCard extends StatefulWidget {
 class _BookCardState extends State<BookCard> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(30),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(60),
-          color: Colors.white.withOpacity(0.9),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.1),
-              blurRadius: 10,
-              spreadRadius: 5,
+    return GestureDetector(
+      onTap: () => showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          content: Padding(
+            padding: const EdgeInsets.all(15),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                BookFieldInformation(
+                  header: 'Title',
+                  content: '${widget.title} ${widget.subtitle!}',
+                ),
+                BookFieldInformation(
+                  header: 'Authors',
+                  content: widget.authors!.join(', '),
+                ),
+                BookFieldInformation(
+                  header: 'Description',
+                  content: widget.description!,
+                ),
+                BookFieldInformation(
+                  header: 'Categories',
+                  content: widget.categories!.join(', '),
+                ),
+              ],
             ),
-          ],
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(21),
+          ),
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(30),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Spacer(),
-              Text(
-                widget.title!,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 15,
-                ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(60),
+            color: Colors.white.withOpacity(0.9),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 10,
+                spreadRadius: 5,
               ),
-              Text(
-                widget.subtitle!,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 15,
-                  color: Colors.grey,
-                ),
-              ),
-              const Spacer(),
-              CachedNetworkImage(
-                imageUrl: widget.imageUrl!,
-                placeholder: (context, url) => const Center(
-                  child: CircularProgressIndicator(),
-                ),
-                errorWidget: (context, url, error) => Icon(
-                  Icons.error,
-                  color: Colors.red.shade700,
-                ),
-                fit: BoxFit.cover,
-                width: 200,
-                height: 200,
-              ),
-              const Spacer(),
-              Text(
-                widget.authors!.join(', '),
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 15,
-                ),
-              ),
-              const Spacer(),
             ],
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(30),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Spacer(),
+                Text(
+                  widget.title!,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+                Text(
+                  widget.subtitle!,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 15,
+                    color: Colors.grey,
+                  ),
+                ),
+                const Spacer(),
+                CachedNetworkImage(
+                  imageUrl: widget.imageUrl!,
+                  placeholder: (context, url) => const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                  errorWidget: (context, url, error) => Icon(
+                    Icons.error,
+                    color: Colors.red.shade700,
+                  ),
+                  fit: BoxFit.cover,
+                  width: 200,
+                  height: 200,
+                ),
+                const Spacer(),
+                Text(
+                  widget.authors!.join(', '),
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+                const Spacer(),
+              ],
+            ),
           ),
         ),
       ),
