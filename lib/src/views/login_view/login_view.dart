7ec37dc80f69ed770e69835f0468557525141871
Mike Zamayias <mzamagias@gmail.com>
2021-12-07 19:58:11 +0200

    login now works

--- lib/src/views/login_view/login_view.dart
+++ lib/src/views/login_view/login_view.dart
@@ -1,46 +1,133 @@
 import 'package:flutter/material.dart';
+import 'package:hmu_library_website/src/views/home_view/home_view.dart';
 
-import '../../widgets/custom_text_form_field.dart';
 import '../../widgets/custom_elevated_button.dart';
+import '../../widgets/custom_text_form_field.dart';
 import '../../widgets/page_blueprint.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+class LoginPage extends StatelessWidget {
+  LoginPage({Key? key}) : super(key: key);
 
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
   final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
+  static final TextEditingController _passwordController =
+      TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return PageBlueprint(
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          CustomTextFormField(
-            controller: _emailController,
-            labelText: 'Email',
-            hintText: 'Enter your email',
-          ),
-          CustomTextFormField(
-            controller: _passwordController,
-            labelText: 'Password',
-            hintText: 'Enter your password',
-          ),
-          Padding(
-            padding: const EdgeInsets.all(15),
-            child: CustomElevatedButton(
-              label: 'Login',
-              onPressed: () => debugPrint('Login'),
+      child: Form(
+        key: formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CustomTextFormField(
+              controller: _emailController,
+              labelText: 'Email',
+              hintText: 'Enter your email',
+              onChanged: (value) => formKey.currentState!.save(),
             ),
-          ),
-        ],
+            CustomTextFormField(
+              controller: _passwordController,
+              labelText: 'Password',
+              hintText: 'Enter your password',
+              onChanged: (value) => formKey.currentState!.save(),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: CustomElevatedButton(
+                label: 'Login',
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    _showDialog(
+                      context,
+                      'Login Successful',
+                      'You have logged in',
+                      [
+                        CustomElevatedButton(
+                          label: 'OK',
+                          onPressed: () => Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  const HomePage(),
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  } else {
+                    _showDialog(
+                      context,
+                      'Error',
+                      'Please enter your email and password',
+                      [
+                        CustomElevatedButton(
+                          label: 'OK',
+                          onPressed: () => Navigator.pop(context),
+                        ),
+                      ],
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
+}
+
+_showDialog(
+  BuildContext context,
+  String title,
+  String message,
+  List<Widget>? actions,
+) {
+  return showGeneralDialog(
+    barrierColor: const Color(0xFF08161C).withOpacity(0.9),
+    barrierDismissible: false,
+    context: context,
+    pageBuilder: (context, animation, secondaryAnimation) => Center(
+      child: AlertDialog(
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+        titleTextStyle: const TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+        content: Text(
+          message,
+          textAlign: TextAlign.center,
+        ),
+        actions: actions,
+        titlePadding: const EdgeInsets.symmetric(
+          horizontal: 45,
+          vertical: 15,
+        ),
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 45,
+          vertical: 15,
+        ),
+        actionsPadding: const EdgeInsets.symmetric(
+          horizontal: 45,
+          vertical: 15,
+        ),
+        actionsAlignment: MainAxisAlignment.center,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(60),
+          side: const BorderSide(
+            color: Color(0xFFA9915D),
+            width: 3.0,
+            style: BorderStyle.solid,
+          ),
+        ),
+      ),
+    ),
+  );
 }
