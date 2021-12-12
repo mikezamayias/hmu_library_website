1b947f4f3a3f10335c976e1862338b48e9afabdd
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2021-12-12 02:00:34 +0200

    refactored, accordion works now.
    accordion logic on when to show and hide fields needs to be developed

--- lib/src/views/sign_up_view/sign_up_view.dart
+++ lib/src/views/sign_up_view/sign_up_view.dart
@@ -1,11 +1,8 @@
-import 'package:flutter/gestures.dart';
+import 'package:accordion/accordion.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_styled_toast/flutter_styled_toast.dart';
+import 'package:intl/intl.dart';
 
-import '../../widgets/custom_elevated_button.dart';
 import '../../widgets/custom_text_form_field.dart';
-import '../../widgets/view_blueprint.dart';
-import '../home_view/home_view.dart';
 
 class SignUpView extends StatefulWidget {
   const SignUpView({Key? key}) : super(key: key);
@@ -15,292 +12,194 @@ class SignUpView extends StatefulWidget {
 }
 
 class _SignUpViewState extends State<SignUpView> {
-  bool _agreed = false;
-  int _currentStep = 0;
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _confirmPasswordController =
-      TextEditingController();
-  final TextEditingController _firstNameController = TextEditingController();
-  final TextEditingController _lastNameController = TextEditingController();
-  final TextEditingController _phoneNumberController = TextEditingController();
-  final TextEditingController _addressController = TextEditingController();
-  final TextEditingController _cityController = TextEditingController();
-  final TextEditingController _countryController = TextEditingController();
-  final TextEditingController _postalCodeController = TextEditingController();
-  final TextEditingController _studentIdController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
-    return ViewBlueprint(
-      child: Form(
-        key: _formKey,
-        child: Stepper(
-          elevation: 15,
-          currentStep: _currentStep,
-          steps: getSteps(),
-          onStepContinue: () {
-            setState(() {
-              if (_currentStep < getSteps().length - 1) {
-                _currentStep = _currentStep + 1;
-              } else {
-                _currentStep = 0;
-              }
-            });
-          },
-          onStepCancel: () {
-            setState(() {
-              if (_currentStep > 0) {
-                _currentStep = _currentStep - 1;
-              } else {
-                _currentStep = 0;
-              }
-            });
-          },
-          onStepTapped: (step) {
-            setState(() {
-              _currentStep = step;
-            });
-          },
-          controlsBuilder: (context, {onStepContinue, onStepCancel}) {
-            return Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                if (_currentStep > 0)
-                  CustomElevatedButton(
-                    label: 'Back',
-                    onPressed: onStepCancel,
-                  ),
-                if (_currentStep < getSteps().length - 1)
-                  CustomElevatedButton(
-                    label: 'Next',
-                    onPressed: onStepContinue,
-                  ),
-                if (_currentStep == getSteps().length - 1)
-                  CustomElevatedButton(
-                    label: 'Sign Up',
-                    onPressed: () {
-                      if (!_formKey.currentState!.validate()) {
-                        showToast(
-                          'You must fill in all fields',
-                          position: StyledToastPosition.center,
-                          backgroundColor: Colors.red.shade700,
-                        );
-                      } else if (!_agreed) {
-                        showToast(
-                          'You must agree to the terms and conditions',
-                          position: StyledToastPosition.center,
-                          backgroundColor: Colors.red.shade700,
-                        );
-                      } else {
-                        _showDialog(
-                          context,
-                          'Sign up successful',
-                          'You have signed up successfully. Please check your email to verify your account.',
-                          [
-                            CustomElevatedButton(
-                              label: 'OK',
-                              onPressed: () => Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (BuildContext context) =>
-                                      const HomeView(),
-                                ),
-                              ),
-                            ),
-                          ],
-                        );
-                      }
-                    },
-                  ),
+    bool _agreed = false;
+    DateTime _dateTime = DateTime.now();
+    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+    final TextEditingController _emailController = TextEditingController();
+    final TextEditingController _passwordController = TextEditingController();
+    final TextEditingController _confirmPasswordController =
+        TextEditingController();
+    final TextEditingController _firstNameController = TextEditingController();
+    final TextEditingController _lastNameController = TextEditingController();
+    final TextEditingController _phoneNumberController =
+        TextEditingController();
+    final TextEditingController _studentIdController = TextEditingController();
+    final TextEditingController _birthdateController = TextEditingController();
+    final TextEditingController _addressController = TextEditingController();
+    final TextEditingController _cityController = TextEditingController();
+    final TextEditingController _countryController = TextEditingController();
+    final TextEditingController _postalCodeController = TextEditingController();
+
+    final CustomTextFormField _emailFormField = CustomTextFormField(
+      width: 300,
+      controller: _emailController,
+      labelText: 'Email',
+      isRequiredField: true,
+      hintText: 'Enter your email',
+      onChanged: (value) => _formKey.currentState!.save(),
+      onFieldSubmitted: (value) => _formKey.currentState!.save(),
+    );
+
+    final CustomTextFormField _passwordFormField = CustomTextFormField(
+      width: 300,
+      controller: _passwordController,
+      labelText: 'Password',
+      isRequiredField: true,
+      hintText: 'Enter your password',
+      onChanged: (value) => _formKey.currentState!.save(),
+      onFieldSubmitted: (value) => _formKey.currentState!.save(),
+    );
+
+    final CustomTextFormField _confirmPasswordFormField = CustomTextFormField(
+      width: 300,
+      controller: _confirmPasswordController,
+      labelText: 'Confirm Password',
+      isRequiredField: true,
+      hintText: 'Confirm your password',
+      onChanged: (value) => _formKey.currentState!.save(),
+      onFieldSubmitted: (value) => _formKey.currentState!.save(),
+      confirmPassword: _passwordController.text,
+    );
+
+    final CustomTextFormField _firstNameFormField = CustomTextFormField(
+      width: 300,
+      controller: _firstNameController,
+      labelText: 'First Name',
+      isRequiredField: true,
+      hintText: 'Enter your first name',
+      onChanged: (value) => _formKey.currentState!.save(),
+    );
+
+    final CustomTextFormField _lastNameFormField = CustomTextFormField(
+      width: 300,
+      controller: _lastNameController,
+      labelText: 'Last Name',
+      isRequiredField: true,
+      hintText: 'Enter your last name',
+      onChanged: (value) => _formKey.currentState!.save(),
+    );
+
+    final CustomTextFormField _phoneNumberFormField = CustomTextFormField(
+      width: 300,
+      controller: _phoneNumberController,
+      labelText: 'Phone Number',
+      isRequiredField: true,
+      hintText: 'Enter your phone number',
+      onChanged: (value) => _formKey.currentState!.save(),
+    );
+
+    final CustomTextFormField _studentIdFormField = CustomTextFormField(
+      width: 300,
+      controller: _studentIdController,
+      labelText: 'Student ID',
+      isRequiredField: true,
+      hintText: 'Enter your student ID',
+      onChanged: (value) => _formKey.currentState!.save(),
+    );
+
+    final CustomTextFormField _birthdateFormField = CustomTextFormField(
+      controller: _birthdateController,
+      labelText: 'Birthdate',
+      hintText: 'Enter your birthdate',
+      isRequiredField: true,
+      onChanged: (value) => _formKey.currentState!.save(),
+      onTap: () async {
+        DateTime? _datePicked = await showDatePicker(
+          context: context,
+          initialDate: DateTime.now(),
+          firstDate: DateTime(1900),
+          lastDate: DateTime(2100),
+        );
+        _birthdateController.text = DateFormat('d/M/y').format(_datePicked!);
+        // _birthdateController.value = _birthdateController.value.copyWith(
+        //   text: DateFormat('d/M/y').format(_datePicked ?? _dateTime),
+        //   selection: TextSelection(
+        //     baseOffset: 0,
+        //     extentOffset: _birthdateController.text.length,
+        //   ),
+        // );
+      },
+    );
+
+    final CustomTextFormField _addressFormField = CustomTextFormField(
+      width: 300,
+      controller: _addressController,
+      labelText: 'Address',
+      isRequiredField: true,
+      hintText: 'Enter your address name and number',
+      onChanged: (value) => _formKey.currentState!.save(),
+    );
+
+    final CustomTextFormField _cityFormField = CustomTextFormField(
+      width: 300,
+      controller: _cityController,
+      labelText: 'City',
+      isRequiredField: true,
+      hintText: 'Enter your city',
+      onChanged: (value) => _formKey.currentState!.save(),
+    );
+
+    final CustomTextFormField _countryFormField = CustomTextFormField(
+      width: 300,
+      controller: _countryController,
+      labelText: 'Country',
+      isRequiredField: true,
+      hintText: 'Enter your country',
+      onChanged: (value) => _formKey.currentState!.save(),
+    );
+
+    final CustomTextFormField _postalCodeFormField = CustomTextFormField(
+      width: 300,
+      controller: _postalCodeController,
+      labelText: 'Postal Code',
+      isRequiredField: true,
+      hintText: 'Enter your postal code',
+      onChanged: (value) => _formKey.currentState!.save(),
+    );
+
+    return Form(
+      key: _formKey,
+      child: Accordion(
+        maxOpenSections: 1,
+        children: [
+          AccordionSection(
+            header: const Text('Credentials'),
+            content: Wrap(
+              children: [
+                _emailFormField,
+                _passwordFormField,
+                _confirmPasswordFormField,
               ],
-            );
-          },
-        ),
+            ),
+          ),
+          AccordionSection(
+            header: const Text('Personal Details'),
+            content: Wrap(
+              children: [
+                _firstNameFormField,
+                _lastNameFormField,
+                _birthdateFormField,
+                _phoneNumberFormField,
+                _studentIdFormField,
+              ],
+            ),
+          ),
+          AccordionSection(
+            header: const Text('Address'),
+            content: Wrap(
+              children: [
+                _addressFormField,
+                _cityFormField,
+                _countryFormField,
+                _postalCodeFormField,
+              ],
+            ),
+          )
+        ],
       ),
     );
-  }
-
-  List<Step> getSteps() {
-    return [
-      Step(
-        title: const Text('Credentials'),
-        content: Wrap(
-          children: [
-            CustomTextFormField(
-              controller: _emailController,
-              labelText: 'Email',
-              isRequiredField: true,
-              hintText: 'Enter your email',
-              onChanged: (value) => _formKey.currentState!.save(),
-              onFieldSubmitted: (value) => _formKey.currentState!.save(),
-            ),
-            CustomTextFormField(
-              controller: _passwordController,
-              labelText: 'Password',
-              isRequiredField: true,
-              hintText: 'Enter your password',
-              onChanged: (value) => _formKey.currentState!.save(),
-              onFieldSubmitted: (value) => _formKey.currentState!.save(),
-            ),
-            CustomTextFormField(
-              controller: _confirmPasswordController,
-              labelText: 'Confirm Password',
-              isRequiredField: true,
-              hintText: 'Confirm your password',
-              onChanged: (value) => _formKey.currentState!.save(),
-              onFieldSubmitted: (value) => _formKey.currentState!.save(),
-              confirmPassword: _passwordController.text,
-            ),
-          ],
-        ),
-      ),
-      Step(
-        title: const Text('Personal Details'),
-        content: Wrap(
-          runAlignment: WrapAlignment.start,
-          crossAxisAlignment: WrapCrossAlignment.start,
-          children: [
-            CustomTextFormField(
-              controller: _firstNameController,
-              labelText: 'First Name',
-              isRequiredField: true,
-              hintText: 'Enter your first name',
-              onChanged: (value) => _formKey.currentState!.save(),
-            ),
-            CustomTextFormField(
-              controller: _lastNameController,
-              labelText: 'Last Name',
-              isRequiredField: true,
-              hintText: 'Enter your last name',
-              onChanged: (value) => _formKey.currentState!.save(),
-            ),
-            CustomTextFormField(
-              controller: _phoneNumberController,
-              labelText: 'Phone Number',
-              isRequiredField: true,
-              hintText: 'Enter your phone number',
-              onChanged: (value) => _formKey.currentState!.save(),
-            ),
-            // add birth date picker
-            CustomTextFormField(
-              controller: _studentIdController,
-              labelText: 'Student ID',
-              isRequiredField: true,
-              hintText: 'Enter your student ID',
-              onChanged: (value) => _formKey.currentState!.save(),
-            ),
-          ],
-        ),
-      ),
-      Step(
-        title: const Text('Address'),
-        content: Wrap(
-          children: [
-            CustomTextFormField(
-              controller: _addressController,
-              labelText: 'Address',
-              isRequiredField: true,
-              hintText: 'Enter your address',
-              onChanged: (value) => _formKey.currentState!.save(),
-            ),
-            CustomTextFormField(
-              controller: _cityController,
-              labelText: 'City',
-              isRequiredField: true,
-              hintText: 'Enter your city',
-              onChanged: (value) => _formKey.currentState!.save(),
-            ),
-            CustomTextFormField(
-              controller: _countryController,
-              labelText: 'Country',
-              isRequiredField: true,
-              hintText: 'Enter your country',
-              onChanged: (value) => _formKey.currentState!.save(),
-            ),
-            CustomTextFormField(
-              controller: _postalCodeController,
-              labelText: 'Postal Code',
-              isRequiredField: true,
-              hintText: 'Enter your postal code',
-              onChanged: (value) => _formKey.currentState!.save(),
-            ),
-          ],
-        ),
-      ),
-      Step(
-        title: const Text('Terms and Conditions'),
-        content: Padding(
-          padding: const EdgeInsets.all(30),
-          child: Wrap(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  GestureDetector(
-                    onTap: () => setState(() => _agreed = !_agreed),
-                    child: _agreed
-                        ? const Icon(
-                            Icons.check_box_rounded,
-                            color: Color(0xFF1A4859),
-                          )
-                        : const Icon(
-                            Icons.check_box_outline_blank_rounded,
-                            color: Color(0xFF1A4859),
-                          ),
-                  ),
-                  RichText(
-                    text: TextSpan(
-                      children: [
-                        const TextSpan(
-                          text: 'By signing up, you agree to our ',
-                          style: TextStyle(
-                            fontSize: 15,
-                          ),
-                        ),
-                        TextSpan(
-                          text: 'Terms of Service',
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 15,
-                          ),
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () => showToast(
-                                  "You agree to Terms of Service",
-                                  context: context,
-                                ),
-                        ),
-                        const TextSpan(
-                          text: ' and ',
-                          style: TextStyle(
-                            fontSize: 15,
-                          ),
-                        ),
-                        TextSpan(
-                          text: 'Privacy Policy',
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 15,
-                          ),
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () => showToast(
-                                  "You agree to Privacy Policy",
-                                  context: context,
-                                ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    ];
   }
 
   _showDialog(
